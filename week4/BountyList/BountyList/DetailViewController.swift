318cd97e6fd72a5911c47b757327dfb4eb03f641
hyeWon <35442383+hyeWon0324@users.noreply.github.com>
2020-12-24 16:44:30 +0900

    cell click present modally

---
+++ week4/BountyList/BountyList/DetailViewController.swift
@@ -0,0 +1,23 @@
+//
+//  DetailViewController.swift
+//  BountyList
+//
+//  Created by user on 2020/12/24.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    //segue present modally 
+    @IBAction func close(_ sender: Any) {
+    }
+    
+
+}
